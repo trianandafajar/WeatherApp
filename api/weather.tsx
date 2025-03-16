13a4aea3bcfb8a56ddc261732d52205dc52d106a
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-16 21:19:07 +0700

    Update Wheater

--- api/weather.tsx
+++ api/weather.tsx
@@ -1,29 +1,31 @@
-import axios from "axios"
+import axios from "axios";
 
-const apiKey = process.env.EXPO_PUBLIC_API_KEY
+const apiKey = process.env.EXPO_PUBLIC_API_KEY;
 
-const forecastEndpoint = (params: any) => `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}&aqi=no&alerts=no`
-const locationEndpoint = (params: any) => `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`
+const forecastEndpoint = (params: any) =>
+  `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}&aqi=no&alerts=no`;
+const locationEndpoint = (params: any) =>
+  `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`;
 
 const apiCall = async (endpoint: string) => {
-    const options = {
-        method: 'GET',
-        url: endpoint
-    }
+  const options = {
+    method: "GET",
+    url: endpoint,
+  };
 
-    try {
-        const response = await axios.request(options)
-        return response.data
-    } catch (error) {
-        console.log("error:", error);
-        return null
-    }
-}
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.log("error:", error);
+    return null;
+  }
+};
 
 export const fetchWeatherForecast = (params: any) => {
-    return apiCall(forecastEndpoint(params))
-}
+  return apiCall(forecastEndpoint(params));
+};
 
 export const fetchLocation = (params: any) => {
-    return apiCall(locationEndpoint(params))
-}
+  return apiCall(locationEndpoint(params));
+};
