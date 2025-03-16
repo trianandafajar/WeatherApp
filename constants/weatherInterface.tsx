b8e5fd25f375e2f6ea4e409397165c7b4a556f12
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-16 21:19:27 +0700

    Update UI

--- constants/weatherInterface.tsx
+++ constants/weatherInterface.tsx
@@ -1,43 +1,43 @@
 export interface Weather {
-    location: {
-        name: string,
-        country: number,
-        localtime: string
-    },
-    current: {
-        temp_c: number;
-        condition: {
+  location: {
+    name: string;
+    country: number;
+    localtime: string;
+  };
+  current: {
+    temp_c: number;
+    condition: {
+      text: string;
+      code: number;
+    };
+    wind_kph: number;
+    humidity: number;
+  };
+  forecast: {
+    forecastday: [
+      {
+        date: string;
+        day: {
+          avgtemp_c: number;
+          condition: {
             text: string;
-            code: number
+          };
         };
-        wind_kph: number,
-        humidity: number
-    },
-    forecast: {
-        forecastday: [
-            {
-                date: string,
-                day: {
-                    avgtemp_c: number,
-                    condition: {
-                        text: string
-                    }
-                },
-                hour: [
-                    {
-                        time: string,
-                        temp_c: number;
-                        condition: {
-                            text: string;
-                        };
-                    }
-                ]
-            }
-        ]
-    }
+        hour: [
+          {
+            time: string;
+            temp_c: number;
+            condition: {
+              text: string;
+            };
+          }
+        ];
+      }
+    ];
+  };
 }
 
 export interface Location {
-    name: string;
-    country: string;
+  name: string;
+  country: string;
 }
